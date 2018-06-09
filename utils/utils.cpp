9b82d5e799c39b8c80cec71a1f2dc48819dbcbee
d57 <d57@mail.ru>
2018-06-09 12:41:59 +0400

    More error handling added for os::exec

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -11,7 +11,9 @@ std::string exec(const char* cmd)
     size_t const buf_len = 128;
     std::array<char, buf_len> buffer;
     std::string result;
-    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+
+    int exit_code = -1;
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), [&exit_code](FILE * f){ exit_code = pclose(f); });
     
     if (!pipe)
         throw std::runtime_error(std::string("Cannot execute the command: ") + cmd);
@@ -21,6 +23,12 @@ std::string exec(const char* cmd)
         if (fgets(buffer.data(), buf_len, pipe.get()) != nullptr)
             result += buffer.data();
     }
+    
+    pipe.reset();
+    
+    if( exit_code != 0)
+        throw std::runtime_error(std::string("Process exit code was error for command: ") + cmd);
+    
     return result;
 }
 
